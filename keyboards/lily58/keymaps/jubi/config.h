9628a3dc1e14ea50dadfb235d6e00353c5202673
Julien Billon <julien.billon@datadoghq.com>
2022-08-30 23:02:34 +0200

    use mod-tap and review layers

--- keyboards/lily58/keymaps/jubi/config.h
+++ keyboards/lily58/keymaps/jubi/config.h
@@ -26,9 +26,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
-#define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 250
-#define TAPPING_TOGGLE 1
+#define TAPPING_TERM 180
+#define TAPPING_TOGGLE 3
+#define IGNORE_MOD_TAP_INTERRUPT
 
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
