28165cdf9fc064373e814c107609cdf76932a653
Julien Billon <julien.billon@datadoghq.com>
2022-09-02 15:57:14 +0200

    remove keylogger and adjust layout

--- keyboards/lily58/keymaps/jubi/keymap.c
+++ keyboards/lily58/keymaps/jubi/keymap.c
@@ -16,20 +16,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  =   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |LShift|   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
- * |------+------+------+------+------+------|BackSP |    | ENTER |------+------+------+------+------+------|
+ * |------+------+------+------+------+------| BackSP|    | ENTER |------+------+------+------+------+------|
  * |LCTRL |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |  \   |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   |Enter | LAlt | LGUI | /Space  /       \RAISE \  | RGUI |LOWER | Del  |
+ *                   | ENTER| LAlt | LGUI | /Space  /       \RAISE \  | RGUI |LOWER | Del  |
  *                   |      |      |      |/ Raise /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 
  [_QWERTY] = LAYOUT(
-  KC_GRV,      KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                                    KC_6,            KC_7,  KC_8,     KC_9,    KC_0,     KC_MINS,
-  KC_TAB,      KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                                    KC_Y,            KC_U,  KC_I,     KC_O,    KC_P,     KC_EQL,
-  KC_LSFT,     KC_A,   KC_S,    KC_D,    KC_F,    LT(_LOWER,KC_G),                         LT(_LOWER,KC_H), KC_J,  KC_K,     KC_L,    KC_SCLN,  KC_QUOT,
-  KC_LCTRL,    KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_BSPC,                  KC_ENT, KC_N,            KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,  KC_BSLS,
-                              KC_ENT, KC_LALT, KC_LGUI,  LT(_RAISE, KC_SPC),   TT(_RAISE), KC_RGUI,   TT(_LOWER),  KC_DEL
+  KC_GRV,        KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                                    KC_6,            KC_7,  KC_8,     KC_9,    KC_0,     KC_MINS,
+  KC_TAB,        KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                                    KC_Y,            KC_U,  KC_I,     KC_O,    KC_P,     KC_EQL,
+  SFT_T(KC_ESC), KC_A,   KC_S,    KC_D,    KC_F,    LT(_LOWER,KC_G),                         LT(_LOWER,KC_H), KC_J,  KC_K,     KC_L,    KC_SCLN,  KC_QUOT,
+  KC_LCTRL,      KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_BSPC,                  KC_ENT, KC_N,            KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,  KC_BSLS,
+                                KC_ENT, KC_LALT, KC_LGUI,  LT(_RAISE, KC_SPC),   TT(_RAISE), KC_RGUI,   TT(_LOWER),  KC_DEL
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   |      |      |      | /       /       \      \  |      |      |      |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
- */
+ */ 
 [_LOWER] = LAYOUT(
   _______, _______,           _______,    _______,         _______, _______,                      _______, _______, _______, _______, _______, _______,
   KC_F1,     KC_F2,             KC_F3,      KC_F4,           KC_F5,   KC_F6,                        KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,
@@ -56,11 +56,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * | ESC  |   !  |   @  |   #  |   $  |   %  |                    |   ^  |   &  |   *  |   (  |   )  |   _  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |   |  |   +  |  (   |  )   |                    | PGUP | HOME |  Up  |  END |      |   +  |
+ * |      |   :  |   _  |   -  |  (   |  )   |                    | PGUP | HOME |  Up  |  END |      |   +  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |   &  |   =  |  {   |  }   |-------.    ,-------| PGDO | Left | Down | Right|   :  |   "  |
+ * |      |   `  |   &  |   =  |  {   |  }   |-------.    ,-------| PGDO | Left | Down | Right|   :  |   "  |
  * |------+------+------+------+------+------|  Del  |    |       |------+------+------+------+------+------|
- * |      |      |   _  |   -  |  [   |  ]   |-------|    |-------|      |      |   <  |   >  |   ?  |   |  |
+ * |      |      |   |  |   +  |  [   |  ]   |-------|    |-------|      |      |   <  |   >  |   ?  |   |  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   |      |      |      | / Space /       \      \  |      |      |      |
  *                   |      |      |      |/       /         \      \ |      |      |      |
@@ -69,9 +69,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_RAISE] = LAYOUT(
    KC_ESC, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS,
-  _______, _______, KC_PIPE, KC_PLUS, KC_LPRN, KC_RPRN,                      KC_PGUP, KC_HOME,   KC_UP,  KC_END, _______, KC_PLUS,
-  _______, _______, KC_AMPR,  KC_EQL, KC_LCBR, KC_RCBR,                      KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_COLN, KC_DQT,
-  _______, _______, KC_UNDS, KC_MINS, KC_LBRC, KC_RBRC,  KC_DEL,    _______, _______, _______, KC_LABK, KC_RABK, KC_QUES, KC_PIPE,
+  _______, KC_COLN, KC_UNDS, KC_MINS, KC_LPRN, KC_RPRN,                      KC_PGUP, KC_HOME,   KC_UP,  KC_END, _______, KC_PLUS,
+  _______,  KC_GRV, KC_AMPR,  KC_EQL, KC_LCBR, KC_RCBR,                      KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_COLN, KC_DQT,
+  _______, _______, KC_PIPE, KC_PLUS, KC_LBRC, KC_RBRC,  KC_DEL,    _______, _______, _______, KC_LABK, KC_RABK, KC_QUES, KC_PIPE,
                              _______, _______, _______,  KC_SPC,    _______, _______, _______, _______
 ),
 /* ADJUST
@@ -114,9 +114,9 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
 // When you add source files to SRC in rules.mk, you can use functions.
 const char *read_layer_state(void);
 const char *read_logo(void);
-void set_keylog(uint16_t keycode, keyrecord_t *record);
-const char *read_keylog(void);
-const char *read_keylogs(void);
+// void set_keylog(uint16_t keycode, keyrecord_t *record);
+// const char *read_keylog(void);
+// const char *read_keylogs(void);
 
 // const char *read_mode_icon(bool swap);
 // const char *read_host_led_state(void);
@@ -127,8 +127,8 @@ bool oled_task_user(void) {
   if (is_keyboard_master()) {
     // If you want to change the display of OLED, you need to change here
     oled_write_ln(read_layer_state(), false);
-    oled_write_ln(read_keylog(), false);
-    oled_write_ln(read_keylogs(), false);
+    // oled_write_ln(read_keylog(), false);
+    // oled_write_ln(read_keylogs(), false);
     //oled_write_ln(read_mode_icon(keymap_config.swap_lalt_lgui), false);
     //oled_write_ln(read_host_led_state(), false);
     //oled_write_ln(read_timelog(), false);
@@ -139,12 +139,12 @@ bool oled_task_user(void) {
 }
 #endif // OLED_ENABLE
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  if (record->event.pressed) {
-#ifdef OLED_ENABLE
-    set_keylog(keycode, record);
-#endif
-    // set_timelog();
-  }
-  return true;
-}
+// bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+//   if (record->event.pressed) {
+// #ifdef OLED_ENABLE
+//     set_keylog(keycode, record);
+// #endif
+//     // set_timelog();
+//   }
+//   return true;
+// }
--- keyboards/lily58/keymaps/jubi/rules.mk
+++ keyboards/lily58/keymaps/jubi/rules.mk
@@ -16,7 +16,7 @@ DYNAMIC_TAPPING_TERM_ENABLE = yes
 SRC +=  ./lib/rgb_state_reader.c \
         ./lib/layer_state_reader.c \
         ./lib/logo_reader.c \
-        ./lib/keylogger.c \
+        # ./lib/keylogger.c \
         # ./lib/mode_icon_reader.c \
         # ./lib/host_led_state_reader.c \
         # ./lib/timelogger.c \
